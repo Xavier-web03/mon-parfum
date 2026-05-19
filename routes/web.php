f69993ddbc867fcbb50be4d826a3fc1a18bcb60d
unknown <samuelalasme09@gmail.com>
2026-05-18 14:32:44 -1200

    Final route fix

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/', function () {
     ]);
 });
 
+// 🔹 AUTH
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 // 🔥 ROUTES ADMIN PROTÉGÉES
 Route::middleware(['auth', AdminOnly::class])->group(function () {
 
